90659b74f05d26f77dee96bf77429afaaaa01db8
ARSHDEEP SINGH <2arshahuja@gmail.com>
2017-07-06 19:44:20 +0530

    Create Zeros and Ones.py

---
+++ numpy/Zeros and Ones.py
@@ -0,0 +1,5 @@
+import numpy,sys
+if sys.version_info[0]>=3: raw_input=input
+a=[int(e) for e in raw_input().split()]
+print(numpy.zeros(tuple(a),dtype=numpy.int))
+print(numpy.ones(tuple(a),dtype=numpy.int))
